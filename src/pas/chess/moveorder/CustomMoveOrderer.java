605ae813a24cc53003ec7f6d03be857f45587f2f
unknown <tg6709032@gmail.com>
2024-10-23 00:10:24 -0400

    passing reallyhard once, heristic and move order modifyed

--- src/pas/chess/moveorder/CustomMoveOrderer.java
+++ src/pas/chess/moveorder/CustomMoveOrderer.java
@@ -1,13 +1,16 @@
 package src.pas.chess.moveorder;
 
-
 // SYSTEM IMPORTS
 import edu.bu.chess.search.DFSTreeNode;
 
+// JAVA IMPORTS
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-
-// JAVA PROJECT IMPORTS
+// PROJECT-SPECIFIC IMPORTS
+import edu.bu.chess.game.move.Move;
+import edu.bu.chess.game.move.MoveType;
 import src.pas.chess.moveorder.DefaultMoveOrderer;
 
 public class CustomMoveOrderer
@@ -21,10 +24,56 @@ public class CustomMoveOrderer
 	 * @param nodes. The nodes to order (these are children of a DFSTreeNode) that we are about to consider in the search.
 	 * @return The ordered nodes.
 	 */
-	public static List<DFSTreeNode> order(List<DFSTreeNode> nodes)
-	{
-		// please replace this!
-		return DefaultMoveOrderer.order(nodes);
-	}
+    public static List<DFSTreeNode> order(List<DFSTreeNode> nodes) {
+        List<DFSTreeNode> highImpactMoves = new LinkedList<>();
+        List<DFSTreeNode> castlingMoves = new LinkedList<>();
+        List<DFSTreeNode> otherMoves = new LinkedList<>();
 
+        for (DFSTreeNode currentNode : nodes) {
+            Move currentMove = currentNode.getMove();
+
+            if (currentMove != null) {
+                MoveType moveCategory = currentMove.getType();
+
+                if (isHighImpact(moveCategory)) {
+                    highImpactMoves.add(currentNode);
+                } else if (isCastlingMove(moveCategory)) {
+                    castlingMoves.add(currentNode);
+                } else {
+                    otherMoves.add(currentNode);
+                }
+            } else {
+                otherMoves.add(currentNode);
+            }
+        }
+
+        List<DFSTreeNode> orderedNodes = new ArrayList<>(highImpactMoves.size() + castlingMoves.size() + otherMoves.size());
+        orderedNodes.addAll(highImpactMoves);
+        orderedNodes.addAll(castlingMoves);
+        orderedNodes.addAll(otherMoves);
+
+        return orderedNodes;
+    }
+
+    /**
+     * Determines if a move type is considered high-impact.
+     *
+     * @param type The type of the move.
+     * @return True if the move is high-impact; false otherwise.
+     */
+    private static boolean isHighImpact(MoveType type) {
+        return type == MoveType.CAPTUREMOVE ||
+               type == MoveType.PROMOTEPAWNMOVE ||
+               type == MoveType.ENPASSANTMOVE;
+    }
+
+    /**
+     * Determines if a move type is a castling move.
+     *
+     * @param type The type of the move.
+     * @return True if the move is a castling move; false otherwise.
+     */
+    private static boolean isCastlingMove(MoveType type) {
+        return type == MoveType.CASTLEMOVE;
+    }
 }
